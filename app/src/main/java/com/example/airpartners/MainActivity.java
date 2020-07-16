2167f9cfa0e7b3759b6bba89e45ee037ad5bd124
Lucky <ljordan51@gmail.com>
2020-07-16 16:13:58 -0700

    updated webviewclient to open external links outside webview

--- app/src/main/java/com/example/airpartners/MainActivity.java
+++ app/src/main/java/com/example/airpartners/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.airpartners;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.webkit.WebSettings;
@@ -14,6 +16,19 @@ public class MainActivity extends AppCompatActivity {
 
     private WebView webView;
 
+    // prevent external links from opening in webview
+    private class MyWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            if ("airpartners-ade.web.app".equals(Uri.parse(url).getHost())) {
+                return false;
+            }
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            startActivity(intent);
+            return true;
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,10 +36,10 @@ public class MainActivity extends AppCompatActivity {
         webView = (WebView) findViewById(R.id.webview);
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
-        webView.setWebViewClient(new WebViewClient());
+        webView.setWebViewClient(new MyWebViewClient());
 
         webView.loadUrl("https://airpartners-ade.web.app/?lang=" + Locale.getDefault().toString());
-}
+    }
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
